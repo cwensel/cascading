8115290b55533181ae8af80010d4dc6cde54302c
Chris K Wensel <chris@wensel.net>
2008-11-26 18:35:33 +0000

    added cascade override

--- src/core/cascading/cascade/Cascade.java
+++ src/core/cascading/cascade/Cascade.java
@@ -121,14 +121,39 @@ public class Cascade implements Runnable
     return flows;
     }
 
+  /**
+   * Method getFlowSkipStrategy returns the current {@link cascading.flow.FlowSkipStrategy} used by this Flow.
+   *
+   * @return FlowSkipStrategy
+   */
   public FlowSkipStrategy getFlowSkipStrategy()
     {
     return flowSkipStrategy;
     }
 
-  public void setFlowSkipStrategy( FlowSkipStrategy flowSkipStrategy )
+  /**
+   * Method setFlowSkipStrategy sets a new {@link cascading.flow.FlowSkipStrategy}, the current strategy, if any, is returned.
+   * If a strategy is given, it will be used as the strategy for all {@link Flow} instances managed by this Cascade instance.
+   * To revert back to consulting the strategies associated with each Flow instance, re-set this value to {@code null}, its
+   * default value.
+   * <p/>
+   * FlowSkipStrategy instances define when a Flow instance should be skipped. The default strategy is {@link cascading.flow.FlowSkipIfSinkStale}
+   * and is inherited from the Flow instance in question. An alternative strategy would be {@link cascading.flow.FlowSkipIfSinkExists}.
+   * <p/>
+   * A FlowSkipStrategy will not be consulted when executing a Flow directly through {@link #start()}
+   * @param flowSkipStrategy
+   * @return FlowSkipStrategy
+   */
+  public FlowSkipStrategy setFlowSkipStrategy( FlowSkipStrategy flowSkipStrategy )
     {
-    this.flowSkipStrategy = flowSkipStrategy;
+    try
+      {
+      return this.flowSkipStrategy;
+      }
+    finally
+      {
+      this.flowSkipStrategy = flowSkipStrategy;
+      }
     }
 
   /**
