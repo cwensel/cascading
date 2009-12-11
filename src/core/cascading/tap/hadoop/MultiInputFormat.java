b0a373b199fa061923ddefb817044f57a87e1985
Chris K Wensel <chris@wensel.net>
2009-12-11 13:23:50 -0800

    Fix for NPE in c.t.h.MultiInputFormat during certain testing scenarios. Also changed proportioning to honor suggested numSplits value.

--- src/core/cascading/tap/hadoop/MultiInputFormat.java
+++ src/core/cascading/tap/hadoop/MultiInputFormat.java
@@ -21,15 +21,25 @@
 
 package cascading.tap.hadoop;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import cascading.CascadingException;
 import cascading.util.Util;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.InputFormat;
+import org.apache.hadoop.mapred.InputSplit;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RecordReader;
+import org.apache.hadoop.mapred.Reporter;
 import org.apache.log4j.Logger;
 import org.jets3t.service.S3ServiceException;
-
-import java.io.IOException;
-import java.util.*;
 
 /**
  * Class MultiInputFormat accepts multiple InputFormat class declarations allowing a single MR job
@@ -179,17 +189,20 @@ public class MultiInputFormat implements InputFormat
       }
 
     // attempt to get splits proportionally sized per input format
-    long[] inputSizes = getInputSizes( inputFormats, jobConfs );
-    long totalSize = sum( inputSizes );
+    long[] inputSplitSizes = getInputSplitSizes( inputFormats, jobConfs, numSplits );
+    long totalSplitSize = sum( inputSplitSizes );
 
-    if( totalSize == 0 )
+    if( totalSplitSize == 0 )
       {
       Arrays.fill( indexedSplits, 1 );
       return collapse( getSplits( inputFormats, jobConfs, indexedSplits ), configs );
       }
 
-    for( int i = 0; i < inputSizes.length; i++ )
-      indexedSplits[ i ] = (int) Math.ceil( (double) numSplits * inputSizes[ i ] / (double) totalSize );
+    for( int i = 0; i < inputSplitSizes.length; i++ )
+      {
+      int useSplits = (int) Math.ceil( (double) numSplits * inputSplitSizes[ i ] / (double) totalSplitSize );
+      indexedSplits[ i ] = useSplits == 0 ? 1 : useSplits;
+      }
 
     return collapse( getSplits( inputFormats, jobConfs, indexedSplits ), configs );
     }
@@ -229,17 +242,16 @@ public class MultiInputFormat implements InputFormat
     return inputSplits;
     }
 
-  private long[] getInputSizes( InputFormat[] inputFormats, JobConf[] jobConfs ) throws IOException
+  private long[] getInputSplitSizes( InputFormat[] inputFormats, JobConf[] jobConfs, int numSplits ) throws IOException
     {
     long[] inputSizes = new long[inputFormats.length];
 
     for( int i = 0; i < inputFormats.length; i++ )
       {
       InputFormat inputFormat = inputFormats[ i ];
-      InputSplit[] splits = inputFormat.getSplits( jobConfs[ i ], 1 );
+      InputSplit[] splits = inputFormat.getSplits( jobConfs[ i ], numSplits );
 
-      for( InputSplit split : splits )
-        inputSizes[ i ] = inputSizes[ i ] + split.getLength();
+      inputSizes[ i ] = splits.length;
       }
 
     return inputSizes;
