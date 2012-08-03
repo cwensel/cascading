28ceb3e5bde7adf322bd3687c1a20638a8891cf4
Chris K Wensel <chris@wensel.net>
2012-08-03 09:36:52 -0700

    Fixed issue where c.t.Tap may fail resolving outgoing fields.

--- src/core/cascading/tap/Tap.java
+++ src/core/cascading/tap/Tap.java
@@ -318,12 +318,11 @@ public abstract class Tap<Config, Input, Output> implements FlowElement, Seriali
       throw new FlowException( "Tap may not have more than one incoming Scope" );
 
     // this allows the incoming to be passed through to the outgoing
-    Fields incomingFields = resolveFields( incomingScopes.iterator().next() );
+    Fields incomingFields = incomingScopes.size() == 0 ? null : resolveFields( incomingScopes.iterator().next() );
 
     if( incomingFields != null &&
       ( isSource() && getSourceFields().equals( Fields.UNKNOWN ) ||
         isSink() && getSinkFields().equals( Fields.ALL ) ) )
-
       return new Scope( incomingFields );
 
     if( count == 1 )
