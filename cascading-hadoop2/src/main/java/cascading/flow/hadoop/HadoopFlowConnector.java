ed83678309f69722e53c1a1c9bfc35fa1b7d92e1
Chris K Wensel <chris@wensel.net>
2013-09-01 21:49:15 -0700

    added deprecated HadoopFlowConnector to ease migration from Hadoop 1.x to Hadoop 2.x libraries

---
+++ cascading-hadoop2/src/main/java/cascading/flow/hadoop/HadoopFlowConnector.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2007-2009 Concurrent, Inc. All Rights Reserved.
+ *
+ * Project and contact information: http://www.cascading.org/
+ *
+ * This file is part of the Cascading project.
+ *
+ * Cascading is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cascading is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cascading.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cascading.flow.hadoop;
+
+import java.util.Map;
+
+import cascading.flow.hadoop2.Hadoop2MR1FlowConnector;
+
+/** Class HadoopFlowConnector has been replaced by {@link Hadoop2MR1FlowConnector}. */
+@Deprecated
+public class HadoopFlowConnector extends Hadoop2MR1FlowConnector
+  {
+  public HadoopFlowConnector()
+    {
+    }
+
+  public HadoopFlowConnector( Map<Object, Object> properties )
+    {
+    super( properties );
+    }
+  }
