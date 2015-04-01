0fa24241bddf4e2c42b4957e3a114e22ebe68a26
Patrick Duin <pduin@expedia.com>
2015-04-01 12:05:20 -0700

    Added support for o.a.h.m.l.CombineFileInputFormat in the Hadoop specific c.t.h.PartitionTap implementation.

--- cascading-hadoop/src/main/shared/cascading/tap/hadoop/PartitionTap.java
+++ cascading-hadoop/src/main/shared/cascading/tap/hadoop/PartitionTap.java
@@ -22,16 +22,23 @@ package cascading.tap.hadoop;
 
 import java.beans.ConstructorProperties;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import cascading.flow.FlowProcess;
 import cascading.tap.SinkMode;
 import cascading.tap.Tap;
 import cascading.tap.TapException;
+import cascading.tap.hadoop.io.CombineInputPartitionTupleEntryIterator;
 import cascading.tap.hadoop.io.HadoopTupleEntrySchemeIterator;
 import cascading.tap.hadoop.io.MultiInputSplit;
 import cascading.tap.hadoop.io.TapOutputCollector;
 import cascading.tap.partition.BasePartitionTap;
 import cascading.tap.partition.Partition;
+import cascading.tuple.Tuple;
+import cascading.tuple.TupleEntryIterableChainIterator;
+import cascading.tuple.TupleEntryIterator;
 import cascading.tuple.TupleEntrySchemeCollector;
 import cascading.tuple.TupleEntrySchemeIterator;
 import org.apache.hadoop.fs.Path;
@@ -195,6 +202,41 @@ public class PartitionTap extends BasePartitionTap<JobConf, RecordReader, Output
     catch( IOException exception )
       {
       throw new TapException( "unable to retrieve child partitions", exception );
+      }
+    }
+
+  @Override
+  public TupleEntryIterator openForRead( FlowProcess<JobConf> flowProcess, RecordReader input ) throws IOException
+    {
+    if( flowProcess.getBooleanProperty( HfsProps.COMBINE_INPUT_FILES, false ) )
+      return new CombinePartitionIterator( flowProcess, input );
+
+    return super.openForRead( flowProcess, input );
+    }
+
+  private class CombinePartitionIterator extends TupleEntryIterableChainIterator
+    {
+    public CombinePartitionIterator( final FlowProcess<JobConf> flowProcess, RecordReader input ) throws IOException
+      {
+      super( getSourceFields() );
+
+      List<Iterator<Tuple>> iterators = new ArrayList<Iterator<Tuple>>();
+
+      if( input == null )
+        throw new IOException( "input cannot be null" );
+
+      String identifier = parent.getFullIdentifier( flowProcess );
+
+      iterators.add( createPartitionEntryIterator( flowProcess, input, identifier ) );
+
+      reset( iterators );
+      }
+
+    private CombineInputPartitionTupleEntryIterator createPartitionEntryIterator( FlowProcess<JobConf> flowProcess, RecordReader input, String parentIdentifier ) throws IOException
+      {
+      TupleEntrySchemeIterator schemeIterator = createTupleEntrySchemeIterator( flowProcess, parent, null, input );
+
+      return new CombineInputPartitionTupleEntryIterator( flowProcess, getSourceFields(), partition, parentIdentifier, schemeIterator );
       }
     }
   }
--- cascading-hadoop/src/test/shared/cascading/tap/hadoop/HadoopTapPlatformTest.java
+++ cascading-hadoop/src/test/shared/cascading/tap/hadoop/HadoopTapPlatformTest.java
@@ -54,6 +54,8 @@ import cascading.scheme.hadoop.TextLine;
 import cascading.tap.MultiSourceTap;
 import cascading.tap.SinkMode;
 import cascading.tap.Tap;
+import cascading.tap.partition.DelimitedPartition;
+import cascading.tap.partition.Partition;
 import cascading.tuple.Fields;
 import cascading.tuple.Tuple;
 import cascading.tuple.TupleEntryCollector;
@@ -63,6 +65,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.InputFormat;
 import org.apache.hadoop.mapred.InputSplit;
 import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.JobInProgress;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.RecordReader;
 import org.junit.Test;
@@ -564,6 +567,47 @@ public class HadoopTapPlatformTest extends PlatformTestCase implements Serializa
     assertEquals( 1, splits.length );
 
     validateLength( source.openForRead( process ), 10 );
+    }
+
+  @Test
+  public void testCombinedPartitionTap() throws Exception
+    {
+    getPlatform().copyFromLocal( inputFileLower );
+
+    Tap source = getPlatform().getDelimitedFile( new Fields( "number", "lower" ), " ", inputFileLower );
+
+    Tap partitionTap = getPlatform().getDelimitedFile( new Fields( "lower" ), "+", getOutputPath( "/combinedpartition/partitioned" ), SinkMode.REPLACE );
+
+    Partition partition = new DelimitedPartition( new Fields( "number" ) );
+    partitionTap = getPlatform().getPartitionTap( partitionTap, partition, 1 );
+
+    Flow firstFlow = getPlatform().getFlowConnector().connect( source, partitionTap, new Pipe( "partition" ) );
+
+    firstFlow.complete();
+
+    // Configure combine inputs for reading from the partition tap
+    Map<Object, Object> properties = getProperties();
+    HfsProps.setUseCombinedInput( properties, true );
+    //set to lots of bytes so the test will combine all input files
+    HfsProps.setCombinedInputMaxSize( properties, 100000000L );
+
+    Tap sink = getPlatform().getDelimitedFile( new Fields( "number", "lower" ), "+", getOutputPath( "/combinedpartition/final" ), SinkMode.REPLACE );
+
+    Flow secondFlow = getPlatform().getFlowConnector( properties ).connect( partitionTap, sink, new Pipe( "copy" ) );
+
+    secondFlow.complete();
+
+    //Asserting we combined all partition files into one mapper
+    if( getPlatform().isUseCluster() )
+      assertEquals( 1, secondFlow.getStats().getCounterValue( JobInProgress.Counter.TOTAL_LAUNCHED_MAPS ) );
+
+    List<Tuple> values = getSinkAsList( secondFlow );
+    assertEquals( 5, values.size() );
+    assertTrue( values.contains( new Tuple( "1", "a" ) ) );
+    assertTrue( values.contains( new Tuple( "2", "b" ) ) );
+    assertTrue( values.contains( new Tuple( "3", "c" ) ) );
+    assertTrue( values.contains( new Tuple( "4", "d" ) ) );
+    assertTrue( values.contains( new Tuple( "5", "e" ) ) );
     }
 
   @Test
