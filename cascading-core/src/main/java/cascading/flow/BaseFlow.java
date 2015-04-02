22298c2f172a5aee272c7f2c157d6ef4d9908245
Chris K Wensel <chris@wensel.net>
2015-04-02 03:58:54 -0700

    Fixed issue where trap c.t.Tap#commitResource() would not get called if c.f.Flow#complete() was not called.

--- cascading-core/src/main/java/cascading/flow/BaseFlow.java
+++ cascading-core/src/main/java/cascading/flow/BaseFlow.java
@@ -930,28 +930,12 @@ public abstract class BaseFlow<Config> implements Flow<Config>
       {
       thread = null;
       throwable = null;
-
-      try
-        {
-        commitTraps();
-
-        if( hasListeners() )
-          {
-          for( SafeFlowListener safeFlowListener : getListeners() )
-            safeFlowListener.throwable = null;
-          }
-        }
-      finally
-        {
-        flowStats.cleanup();
-        }
       }
     }
 
   private void commitTraps()
     {
     // commit all the traps, don't fail on an error
-
     for( Tap tap : traps.values() )
       {
       try
@@ -1195,6 +1179,8 @@ public abstract class BaseFlow<Config> implements Flow<Config>
         flowStats.markSuccessful();
 
       internalClean( stop ); // cleaning temp taps may be determined by success/failure
+
+      commitTraps();
 
       try
         {
