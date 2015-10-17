90dce9e3b2fac14327c27f46988586e9879af9fa
nahguam <davidamaughan@gmail.com>
2015-10-17 16:24:29 -0700

    Updated c.t.p.BasePartitionTap and subsequent PartitionTap sub-classes to allow for partition filters by providing one or more c.t.Fields argument selector and c.o.Filter instance pairs.

--- cascading-hadoop/src/main/shared-io/cascading/tap/hadoop/PartitionTap.java
+++ cascading-hadoop/src/main/shared-io/cascading/tap/hadoop/PartitionTap.java
@@ -83,7 +83,7 @@ public class PartitionTap extends BasePartitionTap<Configuration, RecordReader, 
    * base path and default {@link cascading.scheme.Scheme}, and the partition.
    *
    * @param parent    of type Tap
-   * @param partition of type String
+   * @param partition of type Partition
    */
   @ConstructorProperties({"parent", "partition"})
   public PartitionTap( Hfs parent, Partition partition )
@@ -98,7 +98,7 @@ public class PartitionTap extends BasePartitionTap<Configuration, RecordReader, 
    * {@code openWritesThreshold} limits the number of open files to be output to.
    *
    * @param parent              of type Hfs
-   * @param partition           of type String
+   * @param partition           of type Partition
    * @param openWritesThreshold of type int
    */
   @ConstructorProperties({"parent", "partition", "openWritesThreshold"})
@@ -129,7 +129,7 @@ public class PartitionTap extends BasePartitionTap<Configuration, RecordReader, 
    * is called, typically an issue when used inside a {@link cascading.cascade.Cascade}.
    *
    * @param parent             of type Tap
-   * @param partition          of type String
+   * @param partition          of type Partition
    * @param sinkMode           of type SinkMode
    * @param keepParentOnDelete of type boolean
    */
@@ -149,7 +149,7 @@ public class PartitionTap extends BasePartitionTap<Configuration, RecordReader, 
    * {@code openWritesThreshold} limits the number of open files to be output to.
    *
    * @param parent              of type Tap
-   * @param partition           of type String
+   * @param partition           of type Partition
    * @param sinkMode            of type SinkMode
    * @param keepParentOnDelete  of type boolean
    * @param openWritesThreshold of type int
