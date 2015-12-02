b2a9e9eb0f751c236d83b5bad39c4ea35efdbb06
PUSHPAK GOHEY <impushpak.gohey@gmail.com>
2015-12-02 13:56:11 +0530

    Added test case TestSerializationIssue.java that throws CascadingException: unable to load serializer

---
+++ cascading-hadoop2-mr1/src/test/java/cascading/serializationissue/TestSerializationIssue.java
@@ -0,0 +1,123 @@
+package cascading.serializationissue;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+import org.junit.Test;
+
+import cascading.PlatformTestCase;
+import cascading.flow.Flow;
+import cascading.operation.aggregator.Count;
+import cascading.operation.aggregator.Sum;
+import cascading.pipe.CoGroup;
+import cascading.pipe.Every;
+import cascading.pipe.GroupBy;
+import cascading.pipe.Pipe;
+import cascading.pipe.assembly.Rename;
+import cascading.pipe.joiner.Joiner;
+import cascading.pipe.joiner.MixedJoin;
+import cascading.platform.PlatformRunner.Platform;
+import cascading.platform.hadoop2.Hadoop2MR1Platform;
+import cascading.scheme.hadoop.TextDelimited;
+import cascading.tap.SinkMode;
+import cascading.tap.Tap;
+import cascading.tap.hadoop.Hfs;
+import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
+import cascading.tuple.hadoop.BigDecimalSerialization;
+import cascading.tuple.hadoop.TupleSerializationProps;
+import static data.InputData.textDelimitedFile;
+
+/**
+ * The test case throws below error-
+ * cascading.CascadingException: unable to load serializer for: cascading.platform.hadoop2.ImplicitBigDecimalType from: org.apache.hadoop.io.serializer.SerializationFactory
+ * 
+ * @author pushpakg
+ *
+ */
+
+@Platform({Hadoop2MR1Platform.class})
+public class TestSerializationIssue extends PlatformTestCase {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Test
+	public void testSerializationIssue() throws IOException {
+		Properties props = new Properties();
+
+		TupleSerializationProps.addSerialization(props,
+				BigDecimalSerialization.class.getName());
+		
+		getPlatform().getProperties().putAll(props);
+		
+		/*
+		 * Could not replicate the issue if taps are defined like below
+		 * 
+		 * */
+		/*Tap source = getPlatform().getDelimitedFile(
+				new Fields("account_no", "name"), true, ",", null,
+				new Class[] { ImplicitBigDecimalType.class, String.class },
+				textDelimitedFile, SinkMode.KEEP);
+		
+		Tap sink = getPlatform().getDelimitedFile(
+				new Fields("a.account_no", "b.account_no", "a.sum"),
+				true, ",", null,
+				new Class[] { ImplicitBigDecimalType.class, ImplicitBigDecimalType.class, Double.class },
+				getOutputPath("account_output"), SinkMode.REPLACE);
+		*/
+		/*
+		 * The issue is replicated when taps are defined like below
+		 * 
+		 * */
+		Tap source = new Hfs(new TextDelimited(new Fields("account_no", "name"), true, ",", null, 
+				new Class[] { ImplicitBigDecimalType.class, String.class } ), textDelimitedFile);
+
+		Tap sink = new Hfs(new TextDelimited(new Fields("a.account_no",
+				"b.account_no", "a.sum"), true, ",", null, 
+				new Class[] { ImplicitBigDecimalType.class, ImplicitBigDecimalType.class, Double.class } ), 
+				getOutputPath("serializer_issue_output"), SinkMode.REPLACE);
+		
+
+		Pipe inputPipe = new Pipe("Input Pipe");
+		Fields account_no = new Fields("account_no", ImplicitBigDecimalType.class);
+
+		Pipe groupByPipe = new GroupBy("GroupBy Pipe", inputPipe, account_no);
+
+		Count count = new Count(new Fields("count").applyTypes(ImplicitBigDecimalType.class));
+
+		Pipe countPipe = new Every(groupByPipe, new Fields("name"), count, Fields.ALL);
+
+		Pipe p1 = new Pipe("Pipe P1", countPipe);
+		Pipe p2 = new Pipe("Pipe P2", countPipe);
+
+		p1 = new Rename(p1, new Fields("account_no", "count"), new Fields("a.account_no", "a.count"));
+		p2 = new Rename(p2, new Fields("account_no", "count"), new Fields("b.account_no", "b.count"));
+
+		Pipe p3 = new Pipe("Join P3", p1);
+		Pipe p4 = new Pipe("Join P4", p2);
+
+		Pipe[] joinPipes = new Pipe[] { p3, p4 };
+		boolean[] joinType = new boolean[] { true, true };
+		Joiner joiner = new MixedJoin(joinType);
+
+		Pipe cogroupPipe = new CoGroup(joinPipes,
+				new Fields[] { new Fields("a.account_no"), new Fields("b.account_no") },
+				new Fields("a.account_no", "a.count", "b.account_no", "b.count"),
+				joiner);
+
+		Sum sum = new Sum(new Fields("a.sum"));
+
+		Pipe sumPipe = new Every(cogroupPipe, new Fields("a.count"), sum,
+				Fields.ALL);
+
+		Flow flow = getPlatform().getFlowConnector().connect(source, sink, sumPipe);
+		
+		flow.complete();
+
+		List<Tuple> results = asList(flow, sink, new Fields("a.sum"));
+		assertTrue(results.contains(new Tuple(3.0)));
+	}
+}
