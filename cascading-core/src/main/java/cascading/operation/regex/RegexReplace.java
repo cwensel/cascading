afaaf2434418be47d365647d0733f6e5965cd9d1
Chris K Wensel <chris@wensel.net>
2017-09-15 14:05:42 -0700

    copyright update

--- cascading-core/src/main/java/cascading/operation/regex/RegexReplace.java
+++ cascading-core/src/main/java/cascading/operation/regex/RegexReplace.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2016-2017 Chris K Wensel. All Rights Reserved.
  * Copyright (c) 2007-2017 Xplenty, Inc. All Rights Reserved.
  *
  * Project and contact information: http://www.cascading.org/
